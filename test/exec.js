560faf68396a352ce1d010d0e9021c3f05631854
KieronWiltshire <kieron.wiltshire@outlook.com>
2017-08-01 16:34:58 +0100

    v up

--- test/exec.js
+++ test/exec.js
@@ -1,6 +1,6 @@
 'use strict';
 
 import Gelert from '../src/gelert';
-import executeUnitTests from './index';
+import UnitTests from './index';
 
-executeUnitTests(Gelert);
+describe('gelert', UnitTests(Gelert));
