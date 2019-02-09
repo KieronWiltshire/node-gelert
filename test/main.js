5e07a5d51cec911d9095353758753460679d5315
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2019-02-09 00:47:16 +0000

    moved guard to the lower part of the file as only guard testing is required now

--- test/main.js
+++ test/main.js
@@ -12,40 +12,6 @@ Chai.use(ChaiUUID);
  */
 describe('gelert', function() {
 
-  it('should not create a new instance of {Guard} without specifying a type', function(done) {
-    try {
-      new Guard();
-      done(new Error('An instance of {Guard} was created without specifying a type'));
-    } catch (error) {
-      done();
-    }
-  });
-
-  it('should not create a new instance of {Guard} specifying a type as anything other than a string', function(done) {
-    try {
-      new Guard({ id: 1, type: 1 });
-      done(new Error('An instance of {Guard} was created without specifying a valid type'));
-    } catch (error) {
-      done();
-    }
-  });
-
-  it('should create a new instance of {Guard} with the type being specified as "role"', async function() {
-    let role = new Guard({ id: 1, type: 'role' });
-
-    Chai.expect(role.getId()).to.be.a('number');
-    Chai.expect(role.getId()).to.equal(1);
-    Chai.expect(role.getType()).to.be.a('string');
-    Chai.expect(role.getType()).to.equal('role');
-  });
-
-  it('should create a new instance of {Guard} and create an identifier if one isn\'t specified', async function() {
-    let role = new Guard({ type: 'role' });
-
-    Chai.expect(role.getId()).to.not.be.null;
-    Chai.expect(role.getId()).to.be.a.uuid('v4');
-  });
-
   it('should not create a new instance of {Permission} without a value', function(done) {
     try {
       new Permission();
@@ -132,6 +98,40 @@ describe('gelert', function() {
     Chai.expect(superPermission.isSuper(superPermission3)).to.be.false;
     Chai.expect(superPermission.isSuper(superPermission)).to.be.false;
     Chai.expect(superPermission.isSuper(permission)).to.be.true;
+  });
+
+  it('should not create a new instance of {Guard} without specifying a type', function(done) {
+    try {
+      new Guard();
+      done(new Error('An instance of {Guard} was created without specifying a type'));
+    } catch (error) {
+      done();
+    }
+  });
+
+  it('should not create a new instance of {Guard} specifying a type as anything other than a string', function(done) {
+    try {
+      new Guard({ id: 1, type: 1 });
+      done(new Error('An instance of {Guard} was created without specifying a valid type'));
+    } catch (error) {
+      done();
+    }
+  });
+
+  it('should create a new instance of {Guard} with the type being specified as "role"', async function() {
+    let role = new Guard({ id: 1, type: 'role' });
+
+    Chai.expect(role.getId()).to.be.a('number');
+    Chai.expect(role.getId()).to.equal(1);
+    Chai.expect(role.getType()).to.be.a('string');
+    Chai.expect(role.getType()).to.equal('role');
+  });
+
+  it('should create a new instance of {Guard} and create an identifier if one isn\'t specified', async function() {
+    let role = new Guard({ type: 'role' });
+
+    Chai.expect(role.getId()).to.not.be.null;
+    Chai.expect(role.getId()).to.be.a.uuid('v4');
   });
 
 });
